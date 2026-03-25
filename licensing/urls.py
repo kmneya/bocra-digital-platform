7b2245baf1d76a48c82cf7f50f026774d090a157
Kebadiretse Neya <motlhabi462@gmail.com>
2026-03-25 17:28:25 +0200

    add migration init

--- licensing/urls.py
+++ licensing/urls.py
@@ -7,5 +7,10 @@ urlpatterns = [
     path('apply/aircraft/', views.aircraft_license_form, name='aircraft_license_form'),
     path('apply/cellular/', views.cellular_license_form, name='cellular_license_form'),
     path('<int:pk>/', views.license_detail, name='license_detail'),
+
+    path('officer/', views.officer_license_list, name='officer_license_list'),
+    path('officer/<int:pk>/', views.officer_license_detail, name='officer_license_detail'),
+    path('officer/<int:pk>/action/', views.officer_license_action, name='officer_license_action'),
     path('<int:pk>/review/', views.review_license, name='review_license'),
 ]
+
